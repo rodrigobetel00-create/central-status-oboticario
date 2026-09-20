5f03deea202ec9adec2d7c20242f716e98de9eca
rodrigobetel00-create <rodrigobetel00@gmail.com>
2026-09-20 20:37:07 -0300

    Publica Central de Status como PWA

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "central-status-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
